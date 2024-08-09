8cfdbeb5a2ed06c7afb5224884258dc2bb6dae19
SC <spcaswell@gmail.com>
2024-08-09 14:21:58 -0500

    practice

--- leetcode/121_buy_and_sell_stock/main.py
+++ leetcode/121_buy_and_sell_stock/main.py
@@ -1,7 +1,8 @@
 """
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future
+to sell that stock.
 
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
@@ -45,6 +46,6 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
 
-    print(s.maxProfit([1, 1, 5, 3, 20, 4]))
+    print(s.maxProfit([2, 10, 1, 3, 5, 4]))
     print(s.maxProfit([7, 1, 5, 3, 6, 4]))
     print(s.maxProfit([7, 6, 4, 3, 1]))
